5337626146f75d62d16519c9aadcd1ca6c1ed555
RajyavardhanPadvi <rpadvi2003@gmail.com>
2024-11-17 12:25:16 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+next.config.mjs
+import withPlaiceholder from "@plaiceholder/next";
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            { hostname: 'res.cloudinary.com', protocol: 'https', port: '' },
+            { hostname: "avatars.githubusercontent.com" , protocol: 'https' , port: ''},
+            { hostname:"example.com", protocol:"https", port: ''},
+            { hostname:"imgak.mmtcdn.com", protocol:"https", port: ''}
+        ]
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
+
+export default withPlaiceholder(nextConfig)
