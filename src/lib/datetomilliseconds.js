06cd38c6da28c48a6192575f3bf488552e10e644
RajyavardhanPadvi <rpadvi2003@gmail.com>
2024-11-17 13:12:22 +0530

    Create datetomilliseconds.js

---
+++ src/lib/datetomilliseconds.js
@@ -0,0 +1,16 @@
+dateToMilliseconds.js
+export const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+
+    const dateInMilliseconds = new Date(start.getTime())
+
+    const dates = []
+
+    while(dateInMilliseconds <= end){
+        dates.push(new Date(dateInMilliseconds).getTime())
+        dateInMilliseconds.setDate(dateInMilliseconds.getDate() + 1)
+    }
+
+    return dates
+}
