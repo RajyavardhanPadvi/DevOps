479c42084da2557308e10e0e98815d5dfeaa7b8a
RajyavardhanPadvi <rpadvi2003@gmail.com>
2024-11-17 12:54:31 +0530

    Create sessionprovider.js

---
+++ src/utils/sessionprovider.js
@@ -0,0 +1,14 @@
+sessionProvider.js
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+
+const Provider = ({ children, session }) => {
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
+}
+
+export default Provider
